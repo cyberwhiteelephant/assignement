cb67637f2f061fb7673a089fbdb10918c1bb920d
cyberwhiteelephant <146865165+cyberwhiteelephant@users.noreply.github.com>
2023-12-11 09:30:51 +0530

    Add files via upload

---
+++ assignement01.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int isPasswordValid(char *password)
+{
+    int length = strlen(password);
+    int hasUpperCase = 0;
+    int hasLowerCase = 0;
+    int hasDigit = 0;
+
+
+    if (length < 8 || length > 20)
+        {
+        return 0;
+        }
+
+
+    for (int i = 0; i < length; i++)
+        {
+        if (isupper(password[i]))
+          {
+            hasUpperCase = 1;
+          }
+            else if (islower(password[i]))
+          {
+            hasLowerCase = 1;
+          }
+            else if (isdigit(password[i]))
+          {
+            hasDigit = 1;
+          }
+        }
+
+
+    if (hasUpperCase && hasLowerCase && hasDigit)
+        {
+           return 1;
+        }
+          else
+            {
+              return 0;
+            }
+        }
+
+        int main()
+        {
+          char password[50];
+
+          printf("Enter the password: ");
+          scanf("%s", password);
+
+          if (isPasswordValid(password))
+            {
+               printf("Password is valid.\n");
+            }
+             else
+                {
+                  printf("Password is invalid.\n");
+                }
+
+    return 0;
+}
---
+++ assignement02.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void findMaxMin(int arr[], int size, int *max, int *min) 
+{
+    
+    *max = *min = arr[0];
+     for (int i = 1; i < size; i++) 
+    {
+        if (arr[i] > *max) 
+        {
+           *max = arr[i];  
+        } 
+           else if (arr[i] < *min) 
+           {
+              *min = arr[i];  
+           }
+    }
+}
+
+int main()
+ {
+    int size;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < size; i++) 
+    {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    int max, min;
+
+    findMaxMin(arr, size, &max, &min);
+
+    printf("Maximum element: %d\n", max);
+    printf("Minimum element: %d\n", min);
+
+    return 0;
+}
